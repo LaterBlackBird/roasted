b9f71e940d69e8e47dd6f3b99dc2c785606d88c4
Seth <seth.holland@gmail.com>
2021-12-04 15:15:26 -0600

    changed login page to a modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
-import { useState, useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage';
-import * as sessionActions from './store/session'
-import Navigation from './components/Navigation';
-
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
+  return (
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
