f99ebf1a35c9613b42184692b018c1be4b54b7b7
Seth <seth.holland@gmail.com>
2021-12-03 18:05:43 -0600

    User authentication and validations

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const {  } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+router.post('/test', function (req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
   });
-  
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+router.get('/restore-user', restoreUser,(req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+router.get( '/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+  }
+);
+
 module.exports = router;
