9f847e66cb413830bbaeaed39222cc8ddc465b3c
Seth <seth.holland@gmail.com>
2021-12-04 12:16:38 -0600

    user login component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path='/'>
+        Home
+      </Route>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+      <Route>
+        Temp 404 Page
+      </Route>
+    </Switch>
   );
 }
 
