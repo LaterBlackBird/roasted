d4500350289226924e7157d5c60d3cc432b1189f
Seth <seth.holland@gmail.com>
2021-12-03 18:20:14 -0600

    user login routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,12 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const {  } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
