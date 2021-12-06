d4e6bd87d36fb916c92d141a83b61bdd18b8d551
Seth <seth.holland@gmail.com>
2021-12-06 13:19:26 -0600

    logo and branding added to splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
+import Splash from './components/Splash';
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -17,6 +18,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <Splash />
+          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
