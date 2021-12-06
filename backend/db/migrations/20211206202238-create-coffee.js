3ff4ddd5073cfcce555eb6cf58dbc358cf78d531
Seth <seth.holland@gmail.com>
2021-12-06 15:28:00 -0600

    coffee table setup

---
+++ backend/db/migrations/20211206202238-create-coffee.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Coffees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: "Users"}
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.fn('now')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Coffees');
+  }
+};
---
+++ backend/db/models/coffee.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Coffee = sequelize.define('Coffee', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false},
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    imageUrl: DataTypes.STRING
+  }, {});
+  Coffee.associate = function(models) {
+    // associations can be defined here
+    Coffee.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+  return Coffee;
+};
