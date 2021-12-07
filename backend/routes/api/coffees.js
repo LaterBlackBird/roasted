7b2483d739f2c4ebcd92c1ff9c83704edac92f90
Seth <seth.holland@gmail.com>
2021-12-06 18:22:53 -0600

    coffee CRUD api routes complete and tested in console

--- backend/routes/api/coffees.js
+++ backend/routes/api/coffees.js
@@ -2,42 +2,80 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 // const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Coffee } = require('../../db/models');
-// const { check } = require('express-validator');
-// const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// const validateCoffee = [
-//     check('email')
-//         .exists({ checkFalsy: true })
-//         .isEmail()
-//         .withMessage('Please provide a valid email.'),
-//     check('username')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 4 })
-//         .withMessage('Please provide a username with at least 4 characters.'),
-//     check('username')
-//         .not()
-//         .isEmail()
-//         .withMessage('Username cannot be an email.'),
-//     check('password')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 6 })
-//         .withMessage('Password must be 6 characters or more.'),
-//     handleValidationErrors
-// ];
+const validateCoffee = [
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a valid coffee name.')
+        .isLength({ min: 6 })
+        .withMessage('Name must be at least 6 characters.')
+        .isLength({ max: 30 })
+        .withMessage('Name cannot be more than 30 characters.'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Please provide a description.'),
+    handleValidationErrors
+
+];
 
 // Get all coffees
 router.get('/', asyncHandler(async (req, res) => {
     const allCoffees = await Coffee.findAll();
-    return res.json({allCoffees});
+    return res.json({ allCoffees });
 }));
 
 // Get a single coffee
 router.get('/:id', asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const coffee = await Coffee.findByPk(id);
-    return res.json({ coffee });
+    const coffeeId = req.params.id;
+    const coffee = await Coffee.findByPk(coffeeId);
+    const data = res.json(coffee.name);
+    if (!coffee) {
+        console.log(data.name)
+    } else {
+        console.log('no coffee found')
+    }
 }));
+
+// Create a coffee
+router.post('/', validateCoffee, asyncHandler(async (req, res) => {
+    const { userId, name, description, imageUrl } = req.body;
+    const newCoffee = await Coffee.create({
+        userId,
+        name,
+        description,
+        imageUrl,
+    });
+    res.json({ message: "Successful" });
+}));
+
+// Update a coffee
+router.patch('/:id', validateCoffee, asyncHandler(async (req, res) => {
+    const coffeeId = req.params.id
+    const { name, description, imageUrl } = req.body;
+    const coffeeToUpdate = await Coffee.findByPk(coffeeId);
+
+    await coffeeToUpdate.update({
+        name,
+        description,
+        imageUrl,
+    });
+    return res.json(coffeeToUpdate);
+}));
+
+
+// Delete a single coffee
+router.delete('/:id', asyncHandler(async (req, res) => {
+    const coffeeId = req.params.id;
+    const coffeeToDelete = await Coffee.findByPk(coffeeId);
+    coffeeToDelete.destroy();
+    res.json({ message: "Coffee Deleted" });
+
+}));
+
 
 module.exports = router;
