3cc722489dbf392cc1d3026b3c87fb53d3e47aad
Seth <seth.holland@gmail.com>
2021-12-08 11:44:45 -0600

    added images to seeder coffee information

--- backend/db/seeders/20211206220224-coffee-base.js
+++ backend/db/seeders/20211206220224-coffee-base.js
@@ -6,12 +6,15 @@ module.exports = {
       {
         userId: 1,
         name: 'Honeymoon Mocha',
-        description: 'Honey and white chocolate - a drink for lovers'
+        description: 'Honey and white chocolate - a drink for lovers',
+        imageUrl: 'https://images.unsplash.com/photo-1540620723565-7e32b1e40e94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
       },
       {
         userId: 1,
         name: 'Candy Pecan Latte',
-        description: 'Butter pecan, brown sugar, and cinnamon - a southern staple'
+        description: 'Butter pecan, brown sugar, and cinnamon - a southern staple',
+        imageUrl: 'https://images.unsplash.com/photo-1495774856032-8b90bbb32b32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+
       }
     ], {});
   },
