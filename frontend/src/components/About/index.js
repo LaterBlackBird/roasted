102371c3b99b50b4faff7c57c1c5dea057ff9b7a
Seth <seth.holland@gmail.com>
2021-12-10 17:49:29 -0600

    about me slider complete

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import "./About.css"
 
 
@@ -9,26 +10,36 @@ const AboutPage = () => {
         if (!showAbout) return;
 
         const closeMenu = () => {
-          setShowAbout(false);
+            setShowAbout(false);
         };
 
         document.addEventListener('click', closeMenu);
 
         return () => document.removeEventListener("click", closeMenu);
-      }, [showAbout]);
+    }, [showAbout]);
 
     return (
         <>
             <div className={`about-card ${!showAbout ? 'move-out' : ''}`}>
                 <div className="about-info">
-                    <img className='about-pic' src="https://res.cloudinary.com/dd1ndszow/image/upload/v1639161384/Seth_the_photographer_otencm.png" alt="" />
-                    <div>
-                        words
-                    </div>
+                    <img className='about-pic' src="https://res.cloudinary.com/dd1ndszow/image/upload/v1639161384/Seth_the_photographer_otencm.png" alt="me holding a coffee" />
+                    <p>Seth Holland</p>
+                    <span>
+                        <a href='https://github.com/LaterBlackBird' target="_blank" rel="noopener noreferrer" ><i class="fab fa-github-square" ></i></a>
+                        <a href='https://www.linkedin.com/in/seth-holland/' target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin"></i></a>
+                    </span>
+                    <p>Tech Stack</p>
+                    <img className='tech-badge' src={`https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white`} alt="Node.js" />
+                    <img className='tech-badge' src={`https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB`} alt="React" />
+                    <img className='tech-badge' src={`https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white`} alt="redux" />
+                    <img className='tech-badge' src={`https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white`} alt="Postgresql" />
+                    <img className='tech-badge' src={`https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB`} alt="Express" />
+                    <img className='tech-badge' src={`https://img.shields.io/badge/Sequelize-52B0E7?style=for-the-badge&logo=Sequelize&logoColor=white`} alt="Postgresql" />
+
                 </div>
             </div>
             <div className={`about-tag ${!showAbout ? 'show' : 'hidden'}`} onClick={() => setShowAbout(!showAbout)}>
-                <p id='about-tag-text'>About</p>
+                <p id='about-tag-text'>DEVELOPER</p>
             </div>
         </>
     )
