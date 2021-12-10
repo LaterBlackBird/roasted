cb3604ea667a3686b558e4f37df85cfa95562fed
Seth <seth.holland@gmail.com>
2021-12-10 11:46:03 -0600

    time elapsed since comment was created now appears on comment

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteThisComment, editThisComment } from '../../store/comment';
 import './Comment.css';
@@ -10,11 +10,40 @@ const Comment = ({ comment, checkinId }) => {
     const sessionUser = useSelector(state => state.session.user);
     const [showEditField, setShowEditField] = useState(false);
     const [editedComment, setEditedComment] = useState(comment.comment);
+    const [elapsedTime, setElapsedTime] = useState('')
     const [errors, setErrors] = useState([]);
 
     const showEditToggle = () => {
         setShowEditField(!showEditField)
     }
+
+    useEffect(() => {
+        const commentCreated = new Date(comment.createdAt);
+        const diff = Number(new Date()) - commentCreated;
+        const minute = 60 * 1000;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const month = day * 30;
+        const year = day * 365;
+        switch (true) {
+            case diff < minute:
+                const seconds = Math.round(diff / 1000);
+                return setElapsedTime(`${seconds} ${seconds > 1 ? 'seconds' : 'second'} ago`);
+            case diff < hour:
+                return setElapsedTime(Math.round(diff / minute) + ' minutes ago');
+            case diff < day:
+                return setElapsedTime(Math.round(diff / hour) + ' hours ago');
+            case diff < month:
+                return setElapsedTime(Math.round(diff / day) + ' days ago');
+            case diff < year:
+                return setElapsedTime(Math.round(diff / month) + ' months ago');
+            case diff > year:
+                return setElapsedTime(Math.round(diff / year) + ' years ago');
+            default:
+                return "";
+        };
+    }, [comment.createdAt])
+
 
     const editComment = async () => {
         setErrors([]);
@@ -57,7 +86,10 @@ const Comment = ({ comment, checkinId }) => {
         <div className='comment-container'>
             <i className="far fa-user"></i>
             <div className='commentView-commentInfo'>
-                <p className='commentView-username'>{comment.User.username}:</p>
+                <div className='commentView-commentInfo-header'>
+                    <span className='commentView-username'>{comment.User.username}:</span>
+                    <span className='commentView-elapsedTime'>{elapsedTime}</span>
+                </div>
                 {showEditField &&
                     <div className='commentView-editComment' >
                         <textarea value={editedComment} onChange={(e) => setEditedComment(e.target.value)} placeholder={errors.length ? errors[0] : 'Comment'}></textarea>
